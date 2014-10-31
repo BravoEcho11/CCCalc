6cac70467f8e456abd3d4c2b436a583fe66c976e
BravoEcho11 <CCrain@live.com>
2014-10-31 17:33:09 -0400

    CC-Upload
    Initial Upload to GH

---
+++ CCCalc/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CCCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalcResult(CCCalc.Models.CaclModel viewModel)
+        {
+            //add the two values
+            viewModel.numRes = (viewModel.numOne + viewModel.numTwo);
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ CCCalc/Models/CaclModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CCCalc.Models
+{
+    public class CaclModel
+    {
+        public decimal numOne { get; set; }
+        public decimal numTwo { get; set; }
+        public decimal numRes { get; set; }
+    }
+}
